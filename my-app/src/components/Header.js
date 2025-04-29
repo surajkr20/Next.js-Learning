0447d0e71f04a2c62684b393d8956e581840d4ef
surajkr20 <suraj71308kumar@gmail.com>
2025-04-29 15:58:03 +0530

    project setup

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,12 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
     <div className='flex items-center justify-around'>
-        <div>Home</div>
+        <Link href={'/'}>Home</Link>
         <div className='flex items-center gap-4'>
-            <span>performance</span>
-            <span>Features</span>
+            <Link href={'/performance'}>performance</Link>
+            <Link href={'/features'}>Features</Link>
         </div>
     </div>
   )
---
+++ snippet-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
